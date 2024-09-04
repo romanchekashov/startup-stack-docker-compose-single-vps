f94c7ca795a3d45b819404d42d973c171ed616e9
rchekashov <romanrich89@gmail.com>
2024-09-05 00:42:50 +0300

    backup

--- restart.sh
+++ restart.sh
@@ -5,7 +5,7 @@ echo "Startup Stack redeploying..."
 git pull
 
 docker-compose down          # stop Dockers
-docker-compose up --build -d # build and start Dockers
+docker-compose --env-file .env.local up --build -d # build and start Dockers
 
 docker ps -a
 
